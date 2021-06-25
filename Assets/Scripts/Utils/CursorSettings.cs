c592374007d47f6e3c49ee20627ae22e34caeb69
Christian Viñolo <32016414+aburronDev@users.noreply.github.com>
2021-06-25 22:27:50 +0200

    CursorSettings

---
+++ Assets/Scripts/Utils/CursorSettings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace aburron.Utils
+{
+	public class CursorSettings : MonoBehaviour
+	{
+		[SerializeField] CursorLockMode cursorLockMode = CursorLockMode.Locked;
+		[SerializeField] bool isVisible = false;
+
+		private void Start()
+		{
+			Cursor.lockState = cursorLockMode;
+			Cursor.visible = isVisible;
+		}
+	}
+}
